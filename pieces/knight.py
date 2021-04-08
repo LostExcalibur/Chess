2e3ada248f104616f5262b2d380532d8a03a6faf
LostExcalibur <>
2021-04-09 01:18:57 +0200

    Implémenté le déplacement des cavaliers

--- pieces/knight.py
+++ pieces/knight.py
@@ -1,4 +1,5 @@
 import piece
+from itertools import product
 
 
 class Knight(piece.Piece):
@@ -8,6 +9,17 @@ class Knight(piece.Piece):
 
         self.current_square = position
         self.color = color
+
+    @staticmethod
+    def generate_moves_for_piece(color: int, position: tuple[int, int], board) -> list[tuple[int, int]]:
+        x, y = position
+        moves = list(product([x - 1, x + 1], [y - 2, y + 2])) + list(product([x - 2, x + 2], [y - 1, y + 1]))
+        moves = [(x, y) for x, y in moves if 0 <= x < 8 and 0 <= y < 8 and
+                 (board[y][x] == piece.VIDE or board[y][x].color != color)]
+        return moves
+
+    def generate_all_moves(self, board) -> list[tuple[int, int]]:
+        return self.generate_moves_for_piece(self.color, self.current_square, board)
 
     def __repr__(self):
         return "Black " * (self.color == 1) + "White " * (self.color == 0) + super(Knight, self).__repr__()
